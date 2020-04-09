f15d7f446de491afe21097287c839e8b67cdbf33
YanaNirvana <yana@thefloating.coud>
2020-04-08 22:31:16 -0400

    solved day 2 part 1

---
+++ 2019/exampleTests/test_02.rb
@@ -0,0 +1,38 @@
+require 'rspec'
+require_relative '../solutions/solution_02.rb'
+
+include AOC2019D02
+
+describe 'it parses the input into an array' do
+  let(:example1)  {[1,0,0,0,99]}
+  let(:answer1)   {[2,0,0,0,99]}
+
+  let(:example2)  {[2,3,0,3,99]}
+  let(:answer2)   {[2,3,0,6,99]}
+
+  let(:example3)  {[2,4,4,5,99,0]}
+  let(:answer3)   {[2,4,4,5,99,9801]}
+
+  let(:example4)  {'1,1,1,4,99,5,6,0,99'}
+  let(:answer4)   {[30,1,1,4,2,5,6,0,99]}
+
+  it 'correctly executes opcode 1' do
+    execute_section(example1, 0)
+    expect(example1).to eq answer1
+  end
+
+  it 'correctly exeutes opcode 2 part1' do
+    execute_section(example2, 0)
+    expect(example2).to eq answer2
+  end
+
+  it 'correctly executes opcode 2 part2' do
+    execute_section(example3, 0)
+    expect(example3).to eq answer3
+  end
+
+  it 'correctly executes a multi-sectioned intcode' do
+    expect(solve(example4)).to eq answer4
+  end
+
+end
